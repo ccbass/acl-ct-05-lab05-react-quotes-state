7ce479ba3cec8341848024b0f7b4e07c541be746
Colin Bassett <ccwbassett@gmail.com>
2021-05-18 10:25:08 -0700

    api call

---
+++ src/services/simpsonsApi.js
@@ -0,0 +1,6 @@
+const getQuote = async () => {
+    const rawData = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+    const jsonData = await rawData.json()
+
+    return jsonData[0]
+}
